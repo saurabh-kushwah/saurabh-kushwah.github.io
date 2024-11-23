4f4c4aa562e8b1377027478c0a4495a16430813f
Saurabh Singh Kushwah <>
2024-11-23 10:33:25 +0530

    update site

---
+++ out/_next/static/kuGfVHi0gmvk9SBYIFZLh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-0f12e840c3f8732f.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
