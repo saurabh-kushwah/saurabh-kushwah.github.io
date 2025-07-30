1b9e11924b600d743b5cc953ed0edf06691f17a4
Saurabh Singh Kushwah <>
2025-07-30 02:36:46 +0000

    update site

---
+++ out/_next/static/chunks/app/about/page-6605a4faf1da8b27.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{6911:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1621,23)),Promise.resolve().then(n.t.bind(n,8870,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[870,621,271,155,358],()=>s(6911)),_N_E=e.O()}]);
